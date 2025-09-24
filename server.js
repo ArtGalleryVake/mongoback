60222001cd7db209d83f04e2aa3f18ad698fabb8
utopiaviptravel <utopiaviptravel@gmail.com>
2025-09-24 18:16:54 +0400

    cloud

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ import { fileURLToPath } from 'url';
 
 // --- CLOUDINARY SETUP ---
 import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dryegjume',
+  api_key: '924472115811587',
+  api_secret: 'xAj1DVAvHES_jvgNVSUpUDCcQ9Q',
 });
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
